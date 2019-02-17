4345be6e7828495ed7ec152d5a2e330e21afb78c
Stephen Cook <stephen@stephencookdev.co.uk>
2019-02-17 22:40:25 +0000

    Atempt #5 to get JSFiddle demos working

---
+++ examples/basic/demo.js
@@ -0,0 +1,3 @@
+// This file is used for demoing TShirt Forms example in JSFiddle.
+// All of the JS code is stored in the <script type="text/babel"></script> tag
+// (since JSFiddle struggles handling JSX in its JS field)
